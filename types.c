382fc405ba20069c8a0e06aadf83ec64a98bd7cd
Nachiket Naganure <nachiketun8@gmail.com>
2024-03-07 02:35:58 +0000

    solution

--- types.c
+++ types.c
@@ -5,5 +5,40 @@
 
 int main(int argc, char *argv[])
 {
-    
+    int i;
+    struct stat buf;
+    char *ptr;
+
+    if (argc < 2){
+        fprintf(stderr, "usage: path1 [path2 ...]\n");
+        exit(-1);
+    }
+
+    for (i = 1; i < argc; i++)
+    {
+        printf("%s: ", argv[i]);
+        if (lstat(argv[i], &buf) < 0)
+        {
+            fprintf(stderr, "path error\n");
+            continue;
+        }
+        if (S_ISREG(buf.st_mode))
+            ptr = "regular file";
+        else if (S_ISDIR(buf.st_mode))
+            ptr = "directory";
+        else if (S_ISCHR(buf.st_mode))
+            ptr = "character special file";
+        else if (S_ISBLK(buf.st_mode))
+            ptr = "block special file";
+        else if (S_ISFIFO(buf.st_mode))
+            ptr = "fifo";
+        else if (S_ISLNK(buf.st_mode))
+            ptr = "symbolic link";
+        else if (S_ISSOCK(buf.st_mode))
+            ptr = "socket";
+        else
+            ptr = "** unknown mode **";
+        printf("%s\n", ptr);
+    }
+    exit(0);
 }
